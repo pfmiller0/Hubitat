8866c3af95965dfab184bb2c858c59a4ff0682ae
pfmiller0 <74705534+pfmiller0@users.noreply.github.com>
2022-01-21 16:27:03 -0800

    Added weighed average and general cleanup

--- PurpleAir AQI Virtual Sensor.groovy
+++ PurpleAir AQI Virtual Sensor.groovy
@@ -13,7 +13,7 @@ metadata {
 	)
 	{
 		capability "Sensor"
-        	capability "Polling"
+		capability "Polling"
 
 		attribute "aqi", "number"
 		attribute "category", "string" // Description of current air quality
@@ -22,22 +22,24 @@ metadata {
 	}
 
 	preferences {
-		input name: "X_API_Key", type: "text", title: "PurpleAir API key", required: true, description: "Contact contact@purpleair.com to request an API key"
-		input name: "update_interval", type: "enum", title: "Update interval", required: true, description: "Minutes between updates", options: ["15", "30", "60", "180"], defaultValue: "60"
-		input name: "avg_period", type: "enum", title: "Averaging period", required: true, description: "Readings averaged over what time", options: ["pm2.5", "pm2.5_10minute", "pm2.5_30minute", "pm2.5_60minute", "pm2.5_6hour", "pm2.5_24hour", "pm2.5_1week"], defaultValue: "pm2.5_60minute"
-		input name: "device_search", type: "bool", title: "Search for devices", required: true, description: "If false specify device index to use", defaultValue: true
+		input "X_API_Key", "text", title: "PurpleAir API key", required: true, description: "Contact contact@purpleair.com to request an API key"
+		input "update_interval", "enum", title: "Update interval", required: true, description: "Minutes between updates", options: ["15", "30", "60", "180"], defaultValue: "60"
+		input "avg_period", "enum", title: "Averaging period", required: true, description: "Readings averaged over what time", options: ["pm2.5", "pm2.5_10minute", "pm2.5_30minute", "pm2.5_60minute", "pm2.5_6hour", "pm2.5_24hour", "pm2.5_1week"], defaultValue: "pm2.5_60minute"
+		input "device_search", "bool", title: "Search for devices", required: true, description: "If false specify device index to use", defaultValue: true
 
 		if ( device_search ) {
-			input name: "search_coords", type: "text", title: "Search coordinates [lat, long]", required: true, description: "Coordinates at center of sensor search box", defaultValue: "[32.8662843,-117.2546369]"
-			input name: "search_range", type: "number", title: "Search range", required: true, description: "Size of sensor search box (+/- center of search box coordinates)", defaultValue: 1.5
-			input name: "use_miles", type: "bool", title: "Use miles", required: true, description: "Use kilometers if false", defaultValue: true
+			input "search_coords", "text", title: "Search coordinates [lat, long]", required: true, description: "Coordinates at center of sensor search box", defaultValue: "[" + location.latitude + "," + location.longitude + "]"
+			input "search_range", "decimal", title: "Search range", required: true, description: "Size of sensor search box (+/- center of search box coordinates)", defaultValue: 1.5
+			input "unit", "enum", title: "Unit", required: true, options: ["miles", "kilometers"], defaultValue: "miles"
 		} else {
-			input name: "sensor_index", type: "number", title: "Sensor index", required: true, description: "Select=INDEX in URL when viewing a sensor on map.purpleair.com", defaultValue: 90905
+			input "sensor_index", "number", title: "Sensor index", required: true, description: "Select=INDEX in URL when viewing a sensor on map.purpleair.com", defaultValue: 90905
 		}
+		input "weighted_avg", "bool", title: "Weighted average", required: true, description: "Calculate device average weighted by distance", defaultValue: true
+		input "debugMode", "bool", title: "Debug logging", required: true, defaultValue: false
 	}
 }
 
-// Parse events into attributes. This will never be called but needs to be present in the DTH code.
+// Parse events into attributes. Required for device drivers but not used
 def parse(String description) {
 	log.debug("IQAir: Parsing '${description}'")
 }
@@ -47,6 +49,9 @@ def installed() {
 }
 
 def refresh() {
+	sensorCheck()
+}
+def poll() {
 	sensorCheck()
 }
 
@@ -76,21 +81,25 @@ def uninstalled() {
 
 void sensorCheck() {
 	String url="https://api.purpleair.com/v1/sensors"
+	String query_fields="name,${avg_period},latitude,longitude,confidence"
 	Map httpQuery
+	Float[] coords
 	
 	if ( device_search ) {
-		float[] coords = parseJson(search_coords)
-		float[] dist2deg = distance2degrees(coords[0])
-		float[] range = []
+		coords = parseJson(search_coords)
+		Float[] dist2deg = distance2degrees(coords[0])
+		Float[] range = []
+		
+		//log.debug getCategory(search_range)
 
-		if (use_miles) {
-			range = [(search_range as float)/dist2deg[0], (search_range as float)/dist2deg[1]]
-		} else {
-			range = [((search_range as float)/1.609)/dist2deg[0], ((search_range as float)/1.609)/dist2deg[1]]
+		if ( unit == "miles" ) {
+			range = [(search_range as Float)/dist2deg[0], (search_range as Float)/dist2deg[1]]
+		} else { // Convert to km
+			range = [((search_range as Float)/1.609)/dist2deg[0], ((search_range as Float)/1.609)/dist2deg[1]]
 		}
-		httpQuery = [fields: "name,${avg_period},latitude,longitude", location_type: "0", max_age: 3600, nwlat: coords[0] + range[0], nwlng: coords[1] - range[1], selat: coords[0] - range[0], selng: coords[1] + + range[1]]
+		httpQuery = [fields: query_fields, location_type: "0", max_age: 3600, nwlat: coords[0] + range[0], nwlng: coords[1] - range[1], selat: coords[0] - range[0], selng: coords[1] + + range[1]]
 	} else {
-		httpQuery = [fields: "name,${avg_period},latitude,longitude", location_type: "0", max_age: 3600, show_only: "$sensor_index"]
+		httpQuery = [fields: query_fields, max_age: 3600, show_only: "$sensor_index"]
 	}
 
 	Map params = [
@@ -102,11 +111,11 @@ void sensorCheck() {
 		timeout: 30,
 		ignoreSSLIssues: true
 	]
- 
+
 	//log.debug "params: $params"
 
 	try {
-		asynchttpGet('httpResponse', params, [data: null])
+		asynchttpGet('httpResponse', params, [coords: coords])
 	} catch (SocketTimeoutException e) {
 		log.error("Connection to PurpleAir timed out.")
 	} catch (e) {
@@ -120,16 +129,30 @@ void httpResponse(hubitat.scheduling.AsyncResponse resp, Map data) {
 	String sites
 	
 	if (resp.getStatus() != 200 ) {
-		log.error "HTTP error from purpleair.com: " + resp.getStatus()
+		log.error "HTTP error from PurpleAir: " + resp.getStatus()
 		return
 	}
 	
-	sensorData = resp.getJson().data
-	aqiValue = getPart2_5_AQI(sensorAverage(sensorData, 2))
+	sensorData = resp.getJson().data.findAll {it[5] > 90 }
+	//log.debug sensorData
+
+	if ( debugMode ) {
+		log.debug "sites: ${sensorData.collect { it[1] }}"
+		log.debug "AQIs: ${sensorData.collect { getPart2_5_AQI( Float.parseFloat(it[2])) }}"
+		log.debug "unweighted av aqi: ${getPart2_5_AQI(sensorAverage(sensorData, 2))}"
+		log.debug "weighted av aqi: ${getPart2_5_AQI(sensorAverage(sensorData, 2, data.coords))}"
+	}
+	
+	if ( weighted_avg ) {
+		aqiValue = getPart2_5_AQI(sensorAverage(sensorData, 2, data.coords))
+	} else {
+		aqiValue = getPart2_5_AQI(sensorAverage(sensorData, 2))
+	}
+	
 	AQIcategory = getCategory(aqiValue)
 	
 	sites = sensorData.collect { it[1] }.sort()
-	//sites = sites.substring(1, sites.length() - 1)
+	//sites = sites.substring(1, sites.length() - 1) // Remove brackets around sites?
 	
 	if ( sensorData.size() == 0 ) {
 		log.error "No sensors found in search area"
@@ -137,7 +160,7 @@ void httpResponse(hubitat.scheduling.AsyncResponse resp, Map data) {
 		if (sensorData.size() == 1) {
 			sendEvent(name: "sites", value: sites, descriptionText: "AQI reported from site ${sites}")
 		} else {
-			sendEvent(name: "sites", value: sites, descriptionText: "Reported AQI is average of sites ${sites}")
+			sendEvent(name: "sites", value: sites, descriptionText: "AQI is averaged from ${sensorData.size()} sites ${sites}")
 		}
 		sendEvent(name: "category", value: AQIcategory, descriptionText: "${device.displayName} category is ${AQIcategory}")
 		//sendEvent(name: "aqi", value: aqiValue, unit: "AQI", descriptionText: "${device.displayName} AQI level is ${aqiValue}")
@@ -145,7 +168,7 @@ void httpResponse(hubitat.scheduling.AsyncResponse resp, Map data) {
 	}
 }
 
-float sensorAverage(def sensors, int field) {
+Float sensorAverage(def sensors,Integer field) {
 	Integer count = 0
 	Float sum = 0
     
@@ -154,6 +177,36 @@ float sensorAverage(def sensors, int field) {
 		count = count + 1
 	}
 	return sum / count
+}
+
+Float sensorAverage(def sensors, Integer field, Float[] coords) {
+	Float count = 0.0
+	Float sum = 0.0
+	def distances = []
+	Float nearest = 0.0
+    
+	// Weighted average. First find nearest sensor. Then divide sensors distances by nearest distance to get weights.
+	sensors.each {
+		distances.add(Float.valueOf(distance(coords, [Float.valueOf(it[3]), Float.valueOf(it[4])])))
+	}
+	nearest = distances.min()
+	
+	sensors.eachWithIndex { it, i ->
+	   	Float val = Float.valueOf(it[field])
+	   	Float weight = nearest / distances[i]
+		sum = sum + val * weight
+	   	count = count + weight
+	}
+	return sum / count
+}
+
+Float distance(Float[] a, def b) {
+    Float[] dist2deg = distance2degrees(a[0])
+
+    Float lat_diff = (a[0] - b[0])*dist2deg[0]
+    Float lon_diff = (a[1] - b[1])*dist2deg[1]
+
+    return Math.sqrt(lat_diff**2 + lon_diff**2)
 }
 
 // getAQI and AQILinear functions from https://www.airnow.gov/aqi/aqi-calculator/
@@ -179,8 +232,8 @@ Integer getPart2_5_AQI(Float partCount) {
 	}
 }
 
-Integer AQILinear(int AQIhigh, int AQIlow, float Conchigh, float Conclow, float Concentration) {
-	float a
+Integer AQILinear(Integer AQIhigh, Integer AQIlow, Float Conchigh, Float Conclow, Float Concentration) {
+	Float a
 	
 	a=((Concentration-Conclow)/(Conchigh-Conclow))*(AQIhigh-AQIlow)+AQIlow;
 	
@@ -201,16 +254,16 @@ String getCategory(Integer AQI) {
 	} else if ( AQI > 300 && AQI <= 500) {
 		return "Hazardous"
 	} else if ( AQI > 500 ) {
-		return "Hazardous!!! AQI is off the charts!"
+		return "Extremely hazardous!"
 	} else {
 		return "error"
 	}
 }
 
 // Returns miles per degree for a given latitude
-float[] distance2degrees(float latitude) {	
-	float latMilesPerDegree = 69.172 * Math.cos(Math.toRadians(latitude))
-	float longMilesPerDegree = 69
+Float[] distance2degrees(Float latitude) {	
+	Float latMilesPerDegree = 69.172 * Math.cos(Math.toRadians(latitude))
+	Float longMilesPerDegree = 68.972
 	
 	return [latMilesPerDegree, longMilesPerDegree]
 }
