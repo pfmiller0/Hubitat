7e103815fca41b12ac36a1d174de390ab470ab0e
pfmiller0 <74705534+pfmiller0@users.noreply.github.com>
2022-07-29 16:32:59 -0700

    Remove remove buttons for children apps

--- Ps Light Controls.groovy
+++ Ps Light Controls.groovy
@@ -1,5 +1,5 @@
 /*
- *  P's Light Controls Parent app
+ *  Light Controls Parent app
  *
  *  Based on "Shade Control" example app from Hubitat
  *  https://community.hubitat.com/t/help-combining-three-shades-as-one/57028/12
@@ -27,25 +27,18 @@ def mainPage() {
 	dynamicPage(name: "mainPage", title: " ", install: true, uninstall: false) {
 		section {
 			app(name: "childMultiLevel", appName: "Multi-level Light", namespace: "hyposphere.net", title: "Create New Multi-level Light", multiple: true, displayChildApps: true)
-			paragraph " "
-			href "removePage", title: "Remove all Multi-level Lights", description: ""
-		}
-		section {
 			app(name: "childLightToggle", appName: "Two Light Toggle", namespace: "hyposphere.net", title: "Create New Two Light Toggle", multiple: true, displayChildApps: true)
-			paragraph " "
-			href "removePage", title: "Remove all Two Light Toggles", description: ""
+			app(name: "childWeatherLight", appName: "Weather Light", namespace: "hyposphere.net", title: "Create New Weather Light", multiple: true, displayChildApps: true)
 		}
 		section {
-			app(name: "childWeatherLight", appName: "Weather Light", namespace: "hyposphere.net", title: "Create New Weather Light", multiple: true, displayChildApps: true)
-			paragraph " "
-			href "removePage", title: "Remove all Weather ", description: ""
+			href "removePage", title: "Remove P\'s Light Controls", description: ""
 		}
 	}
 }
 
 def removePage() {
-	dynamicPage(name: "removePage", title: "Remove all Light Controls", install: false, uninstall: true) {
-		section ("WARNING!\n\nRemoving removes all Light Controls\n") {
+	dynamicPage(name: "removePage", title: "Remove all P\'s Light Controls", install: false, uninstall: true) {
+		section ("WARNING!\n\nRemove all P\'s Light Controls?\n") {
 		}
 	}
 }
